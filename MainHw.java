bbc92852eaddc89d880b02acf404f2dcf0cab7b6
Ömer Tayfun <83496181+OmerTayfun@users.noreply.github.com>
2021-05-01 21:55:10 +0300

    Update MainHw.java

--- MainHw.java
+++ MainHw.java
@@ -3,29 +3,27 @@ package homeWorkTwo;
 public class MainHw {
 
 	public static void main(String[] args) {
-		Product product1 = new Product(1, "ASUS Notebook", "8 GB Ram", 8.299);
-		Product product2 = new Product(2, "Monster Notebook", "16 GB Ram", 12.299);
-		Product product3 = new Product(3, "Lenovo Notebook", "12 GB Ram", 10.599);
+		Course course1 = new Course(1, "C# Programlama", "Engin Demiroğ",48);
+		Course course2 = new Course(2, "Java React", "Engin Demiroğ",15);
+		
 
-		Product[] products = { product1, product2, product3 };
+		Course[] courses = { course1, course2,};
 
-		for (Product product : products) {
-			System.out.println(product.id + " " + product.productName + " " + product.productDetail + " "
-					+ product.unitPrice + " TL");
+		for (Course course : courses) {
+			System.out.println(course.id + " " + course.courseName + " " + course.courseInstructor + " %"
+					+ course.courseProcess);
 
 		}
 
-		Category category1 = new Category(1, "Bilgisayar");
-		Category category2 = new Category(2, "Elektronik");
+		Category category1 = new Category(1, "Programlama Dilleri");
+		Category category2 = new Category(2, "Eğitmenler");
 
-		ProductManager productManager = new ProductManager();
-		productManager.addToFavorite(product1);
-		productManager.addToFavorite(product2);
-		productManager.addToFavorite(product3);
-
-		productManager.removeFromFavorites(product1);
-		productManager.removeFromFavorites(product2);
-		productManager.removeFromFavorites(product3);
+		CourseManager courseManager = new CourseManager();
+		courseManager.joinThisCourse(course1);
+		courseManager.joinThisCourse(course2);
+		
+		courseManager.removeFromCourse(course1);
+		courseManager.removeFromCourse(course2);
 	}
 
 }
