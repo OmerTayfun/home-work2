fd50239b64a7fc0d76c6e996f02beb79b33f886b
Ömer Tayfun <83496181+OmerTayfun@users.noreply.github.com>
2021-05-01 21:37:11 +0300

    Update and rename Product.java to Courses.java

---
+++ Courses.java
@@ -0,0 +1,20 @@
+package homeWorkTwo;
+
+public class Courses {
+	
+	int id;
+	String courseName;
+	String courseDetail;
+	double coursePrice;
+	
+	public Course(int id,String productName,String productDetail,double unitPrice) {
+		this.id=id;
+		this.courseName=courseName;
+		this.courseDetail=courseDetail;
+		this.coursePrice=coursePrice;
+		
+		
+	}
+	
+
+}
